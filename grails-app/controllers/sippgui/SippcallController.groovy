886646133f14c11e4d8fe7f33a1a29c55c9d6910
Brian Warner <wbwarnerb@mac.com>
2013-06-12 11:45:48 -0700

    first commit

---
+++ grails-app/controllers/sippgui/SippcallController.groovy
@@ -0,0 +1,17 @@
+package sippgui
+
+class SippcallController {
+
+    def placeCall() { 
+		if(params?.phoneNumber) {
+			def sout = new StringBuffer(), serr = new StringBuffer()
+			def sipp = "sipp -s 12132830920 10.98.2.54 -i 10.98.6.26 -sn uac -r 1 -m 1 -d 1200".execute()
+			sipp.consumeProcessOutput(sout, serr)
+			sipp.waitForOrKill(10000)
+			def output = println "out > $sout error > $serr"
+			//println sipp
+			[view_data:sipp.text, view_data2:output ]
+		}
+		else render "No phone number"
+	}
+}
