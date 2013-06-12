51416c0511e8aa72aaeb301afd8d596388f398d9
Brian Warner <wbwarnerb@mac.com>
2013-06-12 13:55:59 -0700

    fixed dash i

--- grails-app/controllers/sippgui/SippcallController.groovy
+++ grails-app/controllers/sippgui/SippcallController.groovy
@@ -5,7 +5,7 @@ class SippcallController {
     def placeCall() { 
 		if(params?.phoneNumber) {
 			def sout = new StringBuffer(), serr = new StringBuffer()
-			def sipp = "sudo sipp -s 12132830920 10.98.2.54 -i 10.98.6.26 -sn uac -r 1 -m 1 -d 1200".execute()
+			def sipp = "sipp -s 12132830920 10.98.2.54 -i 10.98.2.250 -sn uac -r 1 -m 1 -d 1200".execute()
 			sipp.consumeProcessOutput(sout, serr)
 			sipp.waitForOrKill(10000)
 			def output = println "out > $sout error > $serr"
