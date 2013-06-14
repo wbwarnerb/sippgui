3b87d7ce29fae8f424183e3cdd21cd5b73954cc1
Brian Warner <brian.warner@tollfreeforwarding.com>
2013-06-14 12:58:56 -0700

    added executor

--- grails-app/controllers/sippgui/SippcallController.groovy
+++ grails-app/controllers/sippgui/SippcallController.groovy
@@ -4,13 +4,14 @@ class SippcallController {
 
     def placeCall() { 
 		if(params?.phoneNumber) {
-			def sout = new StringBuffer(), serr = new StringBuffer()
+
 			def sipp = "sipp -s ${params.phoneNumber} 10.98.2.54 -i 10.98.2.250 -sn uac -r ${params.cps} -m ${params.maxCount} -d ${params.delay}".execute()
-			sipp.consumeProcessOutput(sout, serr)
-			sipp.waitFor()
-			def output = println "out > $sout error > $serr"
-			//println sipp
-			[view_data:sout]
+
+			runAsync{
+			
+			println sipp.text
+			[view_data:sipp]
+			}
 		}
 		else render "No phone number"
 	}
