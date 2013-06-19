2166ad1066d80a6e433b93fff9c75c5c5ca7711c
Brian Warner <brian.warner@tollfreeforwarding.com>
2013-06-19 14:05:40 -0700

    changed assertions

--- grails-app/controllers/sippgui/SippcallController.groovy
+++ grails-app/controllers/sippgui/SippcallController.groovy
@@ -30,16 +30,13 @@ class SippcallController {
 	def startUas() {
 
 			def sipp = "sipp -sn uas -d 0 10.98.2.250:5080".execute()
-			def sippcheck = "ps -A | grep sipp".execute()
-			assert sippcheck.text.contains("sipp")
 
 		}
 	
 	def stopUas() {
 		
 					def ksipp = "pkill sipp".execute()
-					def sippcheck = "ps -A | grep sipp".execute()
-					assert !sippcheck.text.contains(null)
+//					def sippcheck = "ps -A | grep sipp".execute()
 		
 				}
 
